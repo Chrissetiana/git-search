6cf54e25ee0f0f0d053ad3dd40f33dc71f994de2
chrissetiana <chrissetiana@gmail.com>
2018-05-28 23:22:44 +0100

    Added menu

--- app/src/main/java/com/chrissetiana/querypage/MainActivity.java
+++ app/src/main/java/com/chrissetiana/querypage/MainActivity.java
@@ -1,9 +1,13 @@
 package com.chrissetiana.querypage;
 
+import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,5 +19,22 @@ public class MainActivity extends AppCompatActivity {
         EditText searchText = findViewById(R.id.search_box);
         TextView searchURL = findViewById(R.id.search_url);
         TextView searchResults = findViewById(R.id.search_results);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int menuItemSelected = item.getItemId();
+        if(menuItemSelected == R.id.action_search) {
+            Context context = MainActivity.this;
+            String message = "Search clicked";
+            Toast.makeText(context, message, Toast.LENGTH_LONG).show();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
